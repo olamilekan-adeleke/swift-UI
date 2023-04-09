35bd843daf8b6cc20ef5263debdc69cf4cd68462
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2023-04-09 18:17:36 +0100

    update

--- UIKitToSwiftUI/UIKitToSwiftUI/TodoApp/TodoUIView.swift
+++ UIKitToSwiftUI/UIKitToSwiftUI/TodoApp/TodoUIView.swift
@@ -19,18 +19,51 @@ struct TodoUIView: View {
 
     var body: some View {
         NavigationView {
-            List($todos) { $todo in
-                HStack {
-                    Text(todo.description)
-                        .strikethrough(todo.done)
-                    Spacer()
-                    Image(systemName: todo.done ? "checkmark.square" : "square")
+            ZStack(alignment: .bottom) {
+                List($todos) { $todo in
+                    HStack {
+                        Text(todo.description)
+                            .strikethrough(todo.done)
+                        Spacer()
+                        Image(systemName: todo.done ? "checkmark.square" : "square")
+                    }
+                    .contentShape(Rectangle())
+                    .onTapGesture { todo.done.toggle() }
                 }
-                .contentShape(Rectangle())
-                .onTapGesture { todo.done.toggle() }
+                InputTodoView(todos: $todos)
             }
             .navigationBarTitle("Todos")
         }
+    }
+}
+
+struct InputTodoView: View {
+    @State private var todoDes = ""
+    @Binding var todos: [TodoModel]
+
+    var body: some View {
+        HStack {
+            TextField("Enter Todo", text: $todoDes)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            Spacer()
+            Button {
+                if todoDes.isEmpty { return }
+
+                let todo = TodoModel(description: todoDes, done: false)
+                todos.append(todo)
+                todoDes = ""
+            } label: {
+                Text("Add")
+                    .padding(.horizontal, 16)
+                    .padding(.vertical, 8)
+                    .foregroundColor(.white)
+                    .background(todoDes.isEmpty ? .gray : .green)
+                    .cornerRadius(5)
+            }
+        }
+        .padding(.horizontal, 10)
+        .padding(.vertical, 15)
+        .background(.gray.opacity(0.3))
     }
 }
 
